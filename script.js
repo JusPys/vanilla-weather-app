74e0fb7494d7f06b227a3e5c9cbfa04b3d9dd404
JusPys <120988314+JusPys@users.noreply.github.com>
2023-04-10 23:22:56 +0200

    Moved city to var in API call

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ function displayWeather(response) {
 }
 
 let apiKey = "34ae1065362d42545661451bda2b8a1f";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=${apiKey}&units=metric`;
+let city = "Warsaw";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayWeather);
