9eee3c3984b9290ac8aea40b43f1680fd3273a40
JusPys <120988314+JusPys@users.noreply.github.com>
2023-04-10 23:03:05 +0200

    Display weather condition for a city (JS)

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function displayWeather(response) {
+  //Checking how to find the element in the API response
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
 
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "34ae1065362d42545661451bda2b8a1f";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayWeather);
