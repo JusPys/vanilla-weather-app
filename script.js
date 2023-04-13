c2afd154cae1d696b074a3433bcac800612303f0
JusPys <120988314+JusPys@users.noreply.github.com>
2023-04-13 22:41:15 +0200

    Added dynamic weather icons

--- script.js
+++ script.js
@@ -28,16 +28,23 @@ function displayWeather(response) {
 
   let dateElement = document.querySelector("#date");
 
+  let iconElement = document.querySelector("#weather-icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   date.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "34ae1065362d42545661451bda2b8a1f";
-let city = "Warsaw";
+let city = "London";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayWeather);
