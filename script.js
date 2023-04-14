31f9c038166561c7ae696413d5f7821bd06db429
JusPys <120988314+JusPys@users.noreply.github.com>
2023-04-14 23:03:20 +0200

    Created search form and button

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function displayWeather(response) {
 }
 
 let apiKey = "34ae1065362d42545661451bda2b8a1f";
-let city = "London";
+let city = "Sydney";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayWeather);
